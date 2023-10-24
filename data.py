e7d6c8e967c633ffa29bf00fa70a0e7478d25a7c
Your Name <you@example.com>
2023-10-24 13:58:31 +1100

    fix

--- data.py
+++ data.py
@@ -1,67 +1,48 @@
 import numpy as np
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset as TorchDataset
 import torch
+
+
+class Dataset(TorchDataset):
+    def __init__(self, train=True):
+        if train:
+            self.Xs = torch.tensor(np.load("Xtr.npy"))
+            self.Ys = torch.tensor(np.load("Str.npy"))
+        else:
+            self.Xs = torch.tensor(np.load("Xts.npy"))
+            self.Ys = torch.tensor(np.load("Yts.npy"))
+        self.path = None
+        self.T = None
+
+    def __len__(self):
+        return len(self.Xs)
+
+    def __getitem__(self, i):
+        return self.Xs[i], self.Ys[i]
 
 
 class FashionMNIST5(Dataset):
     def __init__(self, train=True):
-        super().__init__()
+        super().__init__(train)
         self.path = "datasets/FashionMNIST0.5"
         self.T = torch.tensor(
             [[0.5, 0.2, 0.3], [0.3, 0.5, 0.2], [0.2, 0.3, 0.5]]
         )
-        if train:
-            self.Xs = torch.tensor(np.load("Xtr.npy"))
-            self.Ys = torch.tensor(np.load("Str.npy"))
-        else:
-            self.Xs = torch.tensor(np.load("Xts.npy"))
-            self.Ys = torch.tensor(np.load("Yts.npy"))
-
-    def __len__(self):
-        return len(self.Xs)
-
-    def __getitem__(self, i):
-        return self.Xs[i], self.Ys[i]
 
 
 class FashionMNIST6(Dataset):
     def __init__(self, train=True):
-        super().__init__()
+        super().__init__(train)
         self.path = "datasets/FashionMNIST0.6"
         self.T = torch.tensor(
             [[0.4, 0.3, 0.3], [0.3, 0.4, 0.3], [0.3, 0.3, 0.4]]
         )
-        if train:
-            self.Xs = torch.tensor(np.load("Xtr.npy"))
-            self.Ys = torch.tensor(np.load("Str.npy"))
-        else:
-            self.Xs = torch.tensor(np.load("Xts.npy"))
-            self.Ys = torch.tensor(np.load("Yts.npy"))
-
-    def __len__(self):
-        return len(self.Xs)
-
-    def __getitem__(self, i):
-        return self.Xs[i], self.Ys[i]
 
 
 class CIFAR(Dataset):
     def __init__(self, train=True):
-        super().__init__()
+        super().__init__(train)
         self.path = "datasets/CIFAR"
-        self.T = None
-        if train:
-            self.Xs = torch.tensor(np.load("Xtr.npy"))
-            self.Ys = torch.tensor(np.load("Str.npy"))
-        else:
-            self.Xs = torch.tensor(np.load("Xts.npy"))
-            self.Ys = torch.tensor(np.load("Yts.npy"))
-
-    def __len__(self):
-        return len(self.Xs)
-
-    def __getitem__(self, i):
-        return self.Xs[i], self.Ys[i]
 
 
 if __name__ == "__main__":
