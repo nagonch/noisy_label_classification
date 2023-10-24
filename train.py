bd926c3e1f6bf3fc0590ce0b60dd1c2ccc1f582b
Your Name <you@example.com>
2023-10-24 18:09:34 +1100

    fix

--- train.py
+++ train.py
@@ -1,29 +1,36 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import DataLoader
 from model import ResnetPretrained
-
-model = ResnetPretrained(3, 3)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.01)
+from tqdm import tqdm
+from data import CIFAR, FashionMNIST5, FashionMNIST6
 
 
-X = torch.rand(
-    100,
-    3,
-    25,
-    25,
-)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-y = torch.randint(0, 3, size=(100,))
 
-for epoch in range(100):
-    optimizer.zero_grad()
-    outputs = model(X)
-    loss = criterion(outputs, y)
-    loss.backward()
-    optimizer.step()
-    print(f"Epoch [{epoch + 1}/100], Loss: {loss.item()}")
+def train_backward_correction(
+    model,
+    train_dataloader,
+    n_epochs,
+):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+    for epoch in range(n_epochs):
+        for X, y in tqdm(train_dataloader, leave=False):
+            optimizer.zero_grad()
+            outputs = model(X)
+            loss = criterion(outputs, y)
+            loss.backward()
+            optimizer.step()
+        print(f"Epoch [{epoch + 1}/100], Loss: {loss.item()}")
+    return model
+
 
 if __name__ == "__main__":
-    pass
+    dataset = CIFAR()
+    training_data = DataLoader(dataset, batch_size=100, shuffle=True)
+    model = ResnetPretrained(3, 3).to(device)
+    train_backward_correction(model, training_data, 100)
