6895dfd2960fe96f585a6b6ad056a57b40f3cb01
Your Name <you@example.com>
2023-10-26 16:24:01 +1100

    fix

--- model.py
+++ model.py
@@ -1,21 +1,5 @@
-from torchvision.models import resnet50, ResNet50_Weights
 from torch import nn as nn
 import torch.nn.functional as F
-
-
-# class ResnetPretrained(nn.Module):
-#     def __init__(self, input_channels, out_classes):
-#         super().__init__()
-#         self.in_layer = nn.Conv2d(input_channels, 3, 3)
-#         self.resnet = resnet50(weights=ResNet50_Weights.DEFAULT)
-#         self.out_layer = nn.Linear(1000, out_classes)
-
-#     def forward(self, x):
-#         x = F.relu(self.in_layer(x))
-#         x = F.relu(self.resnet(x))
-#         x = self.out_layer(x)
-
-#         return x
 
 
 class FCN(nn.Module):
@@ -46,6 +30,3 @@ class FCN(nn.Module):
 
 if __name__ == "__main__":
     pass
-    # model = ResnetPretrained(1, 3)
-    # x = torch.rand(2, 1, 35, 35)
-    # print(model(x).shape)
