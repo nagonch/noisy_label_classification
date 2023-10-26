b5b0cfdf0135845260c22a9f6862dbfe38559544
Your Name <you@example.com>
2023-10-26 14:25:39 +1100

    fix

--- data.py
+++ data.py
@@ -17,6 +17,7 @@ class Dataset(TorchDataset):
             self.Xs = torch.tensor(np.load(f"{self.path}/Xts.npy"))
             self.Ys = torch.tensor(np.load(f"{self.path}/Yts.npy"))
         self.Xs = self.Xs.float()
+        self.Xs = (self.Xs - self.Xs.min()) / (self.Xs.max() - self.Xs.min())
         self.Xs = self.Xs.to(device)
         self.Ys = self.Ys.to(device)
 
@@ -32,7 +33,7 @@ class FashionMNIST5(Dataset):
         ).to(device)
 
     def __getitem__(self, i):
-        return self.Xs[i][None], self.Ys[i]
+        return self.Xs[i].reshape(-1), self.Ys[i]
 
 
 class FashionMNIST6(Dataset):
@@ -44,7 +45,7 @@ class FashionMNIST6(Dataset):
         ).to(device)
 
     def __getitem__(self, i):
-        return self.Xs[i][None], self.Ys[i]
+        return self.Xs[i].reshape(-1), self.Ys[i]
 
 
 class CIFAR(Dataset):
@@ -52,14 +53,14 @@ class CIFAR(Dataset):
         super().__init__("datasets/CIFAR", train)
 
     def __getitem__(self, i):
-        return torch.permute(self.Xs[i], (-1, 0, 1)), self.Ys[i]
+        return self.Xs[i].reshape(-1), self.Ys[i]
 
 
 if __name__ == "__main__":
     pass
     # from matplotlib import pyplot as plt
 
-    # one = FashionMNIST6()
+    # one = CIFAR()
     # print(one[0][0].shape)
     # for i in range(len(one)):
     #     plt.imshow(one[i][0], cmap="gray")
