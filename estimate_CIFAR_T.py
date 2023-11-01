758db38289f621d5e936df11b033e52e859324a6
Your Name <you@example.com>
2023-11-01 11:07:15 +1100

    fix

--- estimate_CIFAR_T.py
+++ estimate_CIFAR_T.py
@@ -1,11 +1,33 @@
 import torch
-from model import FCN
+from model import LeNet
 from data import CIFAR
-from train import train
+from tqdm import tqdm
+from torch import optim
+import torch.nn.functional as F
 from torch.utils.data import DataLoader, random_split
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def train(
+    model,
+    train_dataloader,
+    n_epochs,
+    lr=1e-2,
+):
+    optimizer = optim.Adagrad(model.parameters(), lr=lr, lr_decay=1e-6)
+
+    model.train()
+
+    for _ in tqdm(range(n_epochs)):
+        for X, y in train_dataloader:
+            optimizer.zero_grad()
+            output_probabilities = model(X)
+            loss = F.cross_entropy(output_probabilities, y)
+            loss.backward()
+            optimizer.step()
+    return model
 
 
 if __name__ == "__main__":
@@ -15,16 +37,17 @@ if __name__ == "__main__":
     train_dataset = CIFAR()
     T_estimating_size = int(len(train_dataset) * T_estimating_dataset_frac)
 
-    model = FCN(train_dataset[0][0].shape[0], 3).to(device)
+    model = LeNet(
+        3,
+    ).to(device)
     training_data = DataLoader(
         train_dataset, batch_size=train_batch_size, shuffle=True
     )
     model = train(
         model,
-        training_method=None,
         train_dataloader=training_data,
         n_epochs=40,
-        inv_transition=None,
+        lr=1e-3,
     )
 
     t_estimating_dataset, _ = random_split(
@@ -41,4 +64,5 @@ if __name__ == "__main__":
         x_best = P[torch.argmax(P[:, i]), :]
         for j in range(3):
             result_T[i][j] = x_best[j]
+    result_T /= result_T.sum(dim=1, keepdim=True)
     print(result_T)
