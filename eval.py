6179974c201f6108d92fa31642f38e13c914d50c
Your Name <you@example.com>
2023-11-08 12:57:27 +1100

    fix vals

--- eval.py
+++ eval.py
@@ -8,6 +8,8 @@ from torcheval.metrics.classification import (
 )
 from torcheval.metrics import MulticlassF1Score
 import argparse
+import os
+import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -39,7 +41,7 @@ def topk_accuracy(result, answer, topk=1):
     return correct.item()
 
 
-def eval(model_path, dataset_name):
+def eval(models_folder, dataset_name):
     dataset_name_to_object = {
         "CIFAR": CIFAR,
         "FashionMNIST5": FashionMNIST5,
@@ -52,44 +54,63 @@ def eval(model_path, dataset_name):
         ).to(device)
     else:
         model = FCN(dataset[0][0].shape[0], 3).to(device)
-    model.load_state_dict(torch.load(model_path))
-    model.eval()
-    test_data = DataLoader(dataset, batch_size=100, shuffle=False)
-    y_preds = []
-    p_preds = []
-    y_gt = []
-    for X, y in test_data:
-        with torch.no_grad():
-            outputs = model(X)
-        p_preds.append(outputs)
-        y_preds.append(outputs.argmax(axis=1))
-        y_gt.append(y)
-    y_preds = torch.cat(y_preds)
-    y_gt = torch.cat(y_gt)
-    p_preds = torch.cat(p_preds)
+    recalls = []
+    precisions = []
+    f1_vals = []
+    accuracy_vals = []
+    for i, model_path in enumerate(sorted(os.listdir(models_folder))):
+        model.load_state_dict(torch.load(f"{models_folder}/{model_path}"))
+        model.eval()
+        test_data = DataLoader(dataset, batch_size=100, shuffle=False)
+        y_preds = []
+        p_preds = []
+        y_gt = []
+        for X, y in test_data:
+            with torch.no_grad():
+                outputs = model(X)
+            p_preds.append(outputs)
+            y_preds.append(outputs.argmax(axis=1))
+            y_gt.append(y)
+        y_preds = torch.cat(y_preds)
+        y_gt = torch.cat(y_gt)
+        p_preds = torch.cat(p_preds)
 
-    recall = MulticlassRecall(num_classes=3, average="macro")
-    recall.update(y_preds, y_gt)
-    print(f"recall: {recall.compute()}")
+        recall = MulticlassRecall(num_classes=3, average="macro")
+        recall.update(y_preds, y_gt)
+        if i == 0:
+            print(f"Best model by val loss recall: {recall.compute()}")
 
-    precision = MulticlassPrecision(num_classes=3, average="macro")
-    precision.update(y_preds, y_gt)
-    print(f"precision: {precision.compute()}")
+        precision = MulticlassPrecision(num_classes=3, average="macro")
+        precision.update(y_preds, y_gt)
+        if i == 0:
+            print(f"Best model by val loss precision: {precision.compute()}")
 
-    f1 = MulticlassF1Score(num_classes=3, average="macro")
-    f1.update(y_preds, y_gt)
-    print(f"f1: {f1.compute()}")
+        f1 = MulticlassF1Score(num_classes=3, average="macro")
+        f1.update(y_preds, y_gt)
+        if i == 0:
+            print(f"Best model by val loss f1: {f1.compute()}")
 
-    top1_acc_val = topk_accuracy(p_preds, y_gt)
-    print(f"top1 accuracy: {top1_acc_val}")
+        top1_acc_val = topk_accuracy(p_preds, y_gt)
+        if i == 0:
+            print(f"Best model by val loss top1 accuracy: {top1_acc_val}")
+
+        recalls.append(recall.compute())
+        precisions.append(precision.compute())
+        f1_vals.append(f1.compute())
+        accuracy_vals.append(top1_acc_val)
+
+    print(f"Average accuracy over {i+1} models: {np.mean(accuracy_vals)}")
+    print(f"Average recalls over {i+1} models: {np.mean(recalls)}")
+    print(f"Average precisions over {i+1} models: {np.mean(precisions)}")
+    print(f"Average f1 over {i+1} models: {np.mean(f1_vals)}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-model-path", type=str, help="Path to the evaluated model"
+        "-models-folder", type=str, help="Path to the evaluated model"
     )
     parser.add_argument("-dataset-name", type=str, help="Name of the dataset")
     args = parser.parse_args()
 
-    print(eval(args.model_path, args.dataset_name))
+    eval(args.models_folder, args.dataset_name)
