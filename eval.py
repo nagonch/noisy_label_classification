c9f6186b8bcb49a17d0b244804837a36e0f09c1b
Your Name <you@example.com>
2023-11-10 11:50:33 +1100

    fix

--- eval.py
+++ eval.py
@@ -10,6 +10,11 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def metrics(preds, y, eps=1e-9):
+    """
+    1. Estimate precision, recall, accuracy@1 and f1 score
+        by each class for a batch predictions
+    2. Return average value by class
+    """
     max_class = torch.max(y)
     recall_list = []
     precision_list = []
@@ -37,6 +42,11 @@ def metrics(preds, y, eps=1e-9):
 
 
 def eval(models_folder, dataset_name):
+    """
+    Evaluates models in model_folder for given dataset_name.
+    Produces recall, precision, f1 score and top1 accuracy
+        averaged over all classes and batches.
+    """
     dataset_name_to_object = {
         "CIFAR": CIFAR,
         "FashionMNIST5": FashionMNIST5,
@@ -44,10 +54,12 @@ def eval(models_folder, dataset_name):
     }
     dataset = dataset_name_to_object[dataset_name](train=False)
     if dataset_name == "CIFAR":
+        # LeNet for CIFAR for better performance
         model = LeNet(
             3,
         ).to(device)
     else:
+        # Standard fully-connected model for FashionMNIST datasets
         model = FCN(dataset[0][0].shape[0], 3).to(device)
     recalls = []
     precisions = []
@@ -76,16 +88,19 @@ def eval(models_folder, dataset_name):
         f1_vals.append(f1)
         accuracy_vals.append(top1_acc)
 
+    # Estimate mean of each metrics across all batches
     mean_acc = round(np.mean(accuracy_vals), 4)
     mean_rec = round(np.mean(recalls), 4)
     mean_prec = round(np.mean(precisions), 4)
     mean_f1 = round(np.mean(f1_vals), 4)
 
+    # Estimate std of each metrics across all batches
     std_acc = round(np.std(accuracy_vals), 4)
     std_rec = round(np.std(recalls), 4)
     std_prec = round(np.std(precisions), 4)
     std_f1 = round(np.std(f1_vals), 4)
 
+    # Print the metrics
     print(f"Average accuracy over {i+1} models: {mean_acc} +- {std_acc}")
     print(f"Average recalls over {i+1} models: {mean_rec} +- {std_rec}")
     print(f"Average precisions over {i+1} models: {mean_prec} +- {std_prec}")
@@ -94,7 +109,9 @@ def eval(models_folder, dataset_name):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-models-folder", type=str, help="Path to the evaluated model")
+    parser.add_argument(
+        "-models-folder", type=str, help="Path a folder with trained models"
+    )
     parser.add_argument("-dataset-name", type=str, help="Name of the dataset")
     args = parser.parse_args()
 
