5b96cbf2f1a95a1b1443c5651b5e86b404d68124
Your Name <you@example.com>
2023-11-10 11:52:38 +1100

    fix

--- data.py
+++ data.py
@@ -7,17 +7,24 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class Dataset(TorchDataset):
+    """
+    Base class for dataset loading routine
+    """
+
     def __init__(self, path, train=True):
         self.path = path
         self.T = None
         dataset = np.load(path)
         if train:
+            # Train labels
             self.Xs = torch.tensor(dataset["Xtr"])
             self.Ys = torch.tensor(dataset["Str"])
         else:
+            # Test labels
             self.Xs = torch.tensor(dataset["Xts"])
             self.Ys = torch.tensor(dataset["Yts"])
         self.Xs = self.Xs.float()
+        # Standard image preprocessing
         self.Xs = (self.Xs - self.Xs.min()) / (self.Xs.max() - self.Xs.min())
         self.Xs = self.Xs.to(device)
         self.Ys = self.Ys.to(device)
@@ -29,6 +36,7 @@ class Dataset(TorchDataset):
 class FashionMNIST5(Dataset):
     def __init__(self, train=True):
         super().__init__("datasets/FashionMNIST0.5.npz", train)
+        # T value provided for the dataset
         self.T = torch.tensor([[0.5, 0.2, 0.3], [0.3, 0.5, 0.2], [0.2, 0.3, 0.5]]).to(
             device
         )
@@ -40,6 +48,7 @@ class FashionMNIST5(Dataset):
 class FashionMNIST6(Dataset):
     def __init__(self, train=True):
         super().__init__("datasets/FashionMNIST0.6.npz", train)
+        # T value provided for the dataset
         self.T = torch.tensor([[0.4, 0.3, 0.3], [0.3, 0.4, 0.3], [0.3, 0.3, 0.4]]).to(
             device
         )
@@ -51,6 +60,7 @@ class FashionMNIST6(Dataset):
 class CIFAR(Dataset):
     def __init__(self, train=True):
         super().__init__("datasets/CIFAR.npz", train)
+        # T value estimated using anchor points method
         self.T = torch.tensor(
             [
                 [0.36434639, 0.32895993, 0.30669368],
@@ -60,9 +70,9 @@ class CIFAR(Dataset):
         )
 
     def __getitem__(self, i):
+        # Permute to return the channels in correct order
         return torch.permute(self.Xs[i], (2, 0, 1)), self.Ys[i]
 
 
 if __name__ == "__main__":
-    data = CIFAR()
-    print(data[0])
+    pass
